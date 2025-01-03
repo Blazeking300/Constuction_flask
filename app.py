67170f9f1e0745bc414120636a701553c58e7e9f
Blazeking300 <ffret2034@gmail.com>
2025-01-03 17:00:40 +0000

    First commit

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+import os
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/service")
+def service():
+    return render_template('service.html')
+
+@app.route("/service-item")
+def service_item():
+    return render_template('service-item.html')
+
+@app.route("/project")
+def project():
+    return render_template('project.html')
+
+@app.route("/blog")
+def blog():
+    return render_template('blog.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
