ac1e3a04afce731fc5a8d26da7c2fcd9b36fc144
Blazeking300 <ffret2034@gmail.com>
2025-01-12 12:11:01 +0000

    removed pymongo

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import os
-from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
